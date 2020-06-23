84ebbf453bc63d4dab107970579fb4c48e4ae0b0
Igor <Igor-p-1990@mail.ru>
2020-06-23 22:51:33 +0300

    добавленно несколько функций

--- sandbox/src/main/java/ru/ga/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/ga/sandbox/MyFirstProgram1.java
@@ -3,7 +3,31 @@ package ru.ga.sandbox;
 public class MyFirstProgram1 {
 
     public static void main (String[] args){
-        System.out.println("Hello World ");
+        hello("world");
+        hello("user");
+        hello("Igor");
+
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + "=" +area(len) );
+
+        double a = 4;
+        double b = 4;
+        System.out.println("Площадь прямоугоьника со сторонами " + a + " и " + b + " = " +area(a,b) );
     }
+
+    public static void hello (String somebody) {
+
+        System.out.println("Hello " + somebody + " !");
+    }
+
+
+    public static double area(double l) {
+       return l*l;
+    }
+
+    public static double area (double a, double b){
+        return a*b;
+    }
+
 
 }
